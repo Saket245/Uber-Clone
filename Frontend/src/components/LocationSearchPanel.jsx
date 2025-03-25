966b2571da4372f9adbb21e71f503bf407cea1ad
saket <saketbarevadia12@gmail.com>
2025-03-25 08:53:40 +0530

    LoactionSearch UI

--- Frontend/src/components/LocationSearchPanel.jsx
+++ Frontend/src/components/LocationSearchPanel.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
+import LocationOnIcon from '@mui/icons-material/LocationOn';
 
 const LocationSearchPanel = () => {
   return (
-    <div>LocationSearchPanel</div>
+    <div>
+     <div className='d-flex gap-2 mt-2'>
+     <div className='loactionIcon d-flex justify-content-center align-items-center'>
+     <LocationOnIcon />
+     </div>
+     
+      <h6>Plot no. 15/B, Hariram Society, Subhashnagar, Bhavnagar</h6>
+     </div>
+     <div className='d-flex gap-2 mt-3'>
+     <div className='loactionIcon d-flex justify-content-center align-items-center'>
+     <LocationOnIcon />
+     </div>
+     
+      <h6>Plot no. 15/B, Hariram Society, Subhashnagar, Bhavnagar</h6>
+     </div>
+     <div className='d-flex gap-2 mt-3'>
+     <div className='loactionIcon d-flex justify-content-center align-items-center'>
+     <LocationOnIcon />
+     </div>
+     
+      <h6>Plot no. 15/B, Hariram Society, Subhashnagar, Bhavnagar</h6>
+     </div>
+     <div className='d-flex gap-2 mt-3'>
+     <div className='loactionIcon d-flex justify-content-center align-items-center'>
+     <LocationOnIcon />
+     </div>
+     
+      <h6>Plot no. 15/B, Hariram Society, Subhashnagar, Bhavnagar</h6>
+     </div>
+    </div>
   )
 }
 
